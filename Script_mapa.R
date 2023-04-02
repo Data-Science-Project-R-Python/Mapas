c0c67172a40ed961be4558c5c61a92104caab106
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-04-01 21:57:11 -0500

    Se ordenó el script de los mapas, se agregó una carpeta con las imágenes del proyecto

--- Script_mapa.R
+++ Script_mapa.R
@@ -1,16 +1,16 @@
-install.packages("mapsPERU")
-install.packages("sf")
-install.packages("mapview")
-install.packages("ggrepel")
 install.packages("plotly")
+install.packages("mapsPERU")
+## Información del paquete https://github.com/musajajorge/mapsPERU 
 install.packages("leaflet")
 install.packages("leaflet.extras")
 install.packages("rworldxtra")
 install.packages("raster")
+install.packages("mapview")
+install.packages("sf")
+install.packages("ggplot2")
 
 library(plotly)
 library(mapsPERU)
-library(ggrepel)
 library(ggplot2)
 library(leaflet)
 library(leaflet.extras)
@@ -20,91 +20,78 @@ library(raster)
 library(sf)
 library(tidyverse)
 
-###############################################################
-df <- map_DEP
+# Utilizamos el datasets "map_DEP" incluida en el paquete "mapsPERU", que incluyen los siguientes datos:
+ 
+# COD_DEPARTAMENTO: Código del departamento
+# DEPARTAMENTO: Nombre del departamento
+# coords_x: Longitud del centroide del departamento
+# coords_y: Latitud del centroide del departamento
+# geometría: MULTIPOLYGON Objeto geométrico
 
-df_1 <- mutate_if(df, is.character, toupper)
-df_1$DEPARTAMENTO <- chartr('??,??,??,??,??','A,E,I,O,U', df_1$DEPARTAMENTO)
+df <- map_DEP
 
 ggplot(df, aes(geometry=geometry)) +
   geom_sf(aes(fill=DEPARTAMENTO)) +
   geom_text(data = df, aes(coords_x, coords_y, group=NULL, label=DEPARTAMENTO), size=2.5)+
   labs(x="", y="")
 
+# Edición de la data: cambiamos la columna de DEPARTAMENTO a mayúscula y sin tilde
+df_1 <- mutate_if(df, is.character, toupper)
 
-#############################################################
+df_1$DEPARTAMENTO <- chartr('ÁÉÍÓÚ', "AEIOU", df_1$DEPARTAMENTO)
 
-dg <- st_read("BAS_LIM_DEPARTAMENTO.shp")
-dk <- st_read("E_2023.shp")
-
-##mapview::mapview(dg,legend = TRUE, labels=F)
-##INFO https://r-spatial.github.io/mapview/articles/mapview_01-basics.html
-
-dg <- dg %>% mutate(centroid = map(geometry, st_centroid), 
-                    coords = map(centroid, st_coordinates), 
-                    coords_x = map_dbl(coords,1), 
-                    coords_y = map_dbl(coords, 2))
-
-
-ingresantes2022 <- read_delim("ingresante_2022.csv", 
-                              delim = "|", escape_double = FALSE, locale = locale(encoding = "ASCII"), 
-                              trim_ws = TRUE)
-
-#ingresantes2022 %>% 
-#  select(DEPARTAMENTO_FILIAL) %>% 
-#  count(DEPARTAMENTO_FILIAL) %>% 
-#  print(n = Inf)
-
-library(readr)
-
+# Importamos la base de datos
 ingresantes <- readxl::read_excel("Ingresantes2022.xlsx")
 
+# Unimos la base de datos que tiene los centroides con la columna de datos de los ingresantes 2021 
 mapa <- df_1 %>% 
-  left_join(ingresantes)
+  left_join(ingresantes) 
 
+# Creamos la base del mapa
+leaflet() %>% addTiles()
 
-
-#addAwesomeMarkers
-
+# Agregamos etiquetas al mapa
 leaflet() %>% addTiles() %>% 
-  addCircles(data = df, lat = ~ coords_x, 
-             lng = ~ coords_y)
+  addCircles(data = mapa, lat = ~coords_y, 
+             lng = ~coords_x)
 
-leaflet() %>% addTiles() %>% 
-  addAwesomeMarkers(data = mapa, lat = ~Latitud, 
-             lng = ~Longitud)
-  
-#addCircleMarkers
+# Encapsulamos los nombres
+regiones <- mapa$DEPARTAMENTO %>% unique()
 
-#datamap <- left_join(df_1, ENA2019, by = "DISTRITO")
+# Definimos colores para las macroregiones
 
-leaflet() %>% addTiles() %>% 
-  addAwesomeMarkers(data = mapa, lat = ~coords_y, lng = ~coords_x)
+Colores <- c('#4daf4a', "#F38458", 
+             "#E6D04F", "#E6D04F", 
+             "#EB7999", "#F38458", 
+             "#5195A9", "#E6D04F", 
+             "#EB7999", "#EB7999",
+             "#EB7999", "#EB7999", 
+             "#F38458", "#F38458",
+             "#5195A9", '#4daf4a',
+             '#4daf4a', "#E6D04F",
+             "#EB7999", "#F38458", 
+             "#E6D04F", '#4daf4a', 
+             "#E6D04F", "#F38458",
+             '#4daf4a')
 
-Colores <- c('#e41a1c', '#377eb8', '#4daf4a', )
-pal <- colorFactor(Colores, domain = REGIONES)
 
-leaflet() %>% addTiles() %>% addCircles(data = mapa, lat = ~coords_y, 
-                                        lng = ~coords_x, 
-                                        fillOpacity = 0.5, label = ~INGRESANTES, 
+# Se vincula la paleta de colores con los nombres de las regiones
+pal <- colorFactor(Colores, domain = regiones)
+
+# Se crean las etiquetas con la cantidad de los ingresantes según los departamentos
+p <- leaflet() %>% addTiles() %>% addCircleMarkers(data = mapa, lat = ~coords_y, radius = 7,
+                                        lng = ~coords_x, color = ~pal(DEPARTAMENTO),
+                                        fillOpacity = 1.8, label = ~INGRESANTES, 
                                         group = "Codigo")
 
+# Generamos las leyendas y la opción de ver el código o la leyenda 
 
-p <- ggplot(mapa, aes(geometry=geometry)) +
-  geom_sf(aes(fill=INGRESANTES)) +
-  geom_text(data = mapa, aes(coords_x, coords_y, group=NULL, label=INGRESANTES), size=2.2)+
-  labs(x="", y="")
+p <- p %>% addLegend(data = mapa, "bottomright", pal = pal, 
+                     values = ~DEPARTAMENTO, title = "Regiones", 
+                     opacity = 0.8, group = "Leyenda")
+p
 
-ggplotly(p, width = 700, height = 700, tooltip = c("NOMBDEP"))
-
-
-p <- ggplot(mapa) + 
-  geom_sf(aes(fill = INGRESANTES)) +
-  geom_text(data = mapa, aes(coords_x, coords_y, group=NULL ,label = NOMBDEP), size = 2.15)
-
-ggplotly(p, width = 700, height = 700, tooltip = c("INGRESANTES"))
-
-
-
-
+p <- p %>% addLayersControl(overlayGroups = c("Codigo", "Leyenda"), 
+                            options = layersControlOptions(collapsed = F))
+p
 
